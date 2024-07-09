fa5aaea41a1809d459d45eb63d1ac57d89f46e65
aMariaR <mariaritapiekas.com>
2024-07-09 13:02:36 -0300

    atualização

---
+++ simulador .asm
@@ -0,0 +1,434 @@
+# Projeto: Simulador do processador MIPS
+# Arquivo: simulador.asm
+# Descrição:
+# Autores: Maria Rita Piekas e Nathália Zófoli
+# data da criação: 09/04/2024
+.text
+
+main:
+	jal 	aberturaArquivo
+	jal 	leituraArquivo
+	jal 	fechaArquivo
+	jal 	aberturaArquivoData
+	jal 	leituraArquivoData
+	jal 	fechaArquivo
+	jal 	inicioBuscaInstrucao
+	li $s7, 0       # Inicializa o contador ($s7)
+	loop_main:
+	jal	buscaInstrucao
+	jal	decodificaInstrucao
+	jal 	executaInstrucao
+	# Incrementa o contador
+        lw $t1, count   # Carrega o valor atual de count para $t1
+        addi $s7, $s7, 1    # Incrementa $s7
+        sw $s7, count   # Armazena de volta em count
+    
+        # Verifica se o contador atingiu 4
+        li $t2, 4       # Carrega o valor 4 para $t2
+        bne $s7, $t2, loop  # Se $s7 != $t2, volta para loop
+	jal     encerraPrograma
+##############################################
+aberturaArquivo:
+	la $a0, local_arquivo 	  	# carrega o endereço do arquivo em $a0
+	li $a1, 0 		  	# seta a flag do arquivo em 0 para modo de leitura
+	la $a0, local_arquivo 	  	# carrega o nome do arquivo em $a0
+	li $v0, 13 		  	# 0 = abre o arquivo em modo de leitura
+	syscall			  	# faz a chamada de sistema n. 13 para abrir o arquivo
+	la $t0, descritor_arquivo 	# armazena em $t0 o endereço do descritor do arquivo
+	sw $v0, 0($t0)		  	# armazena em descritor_arquivo o valor de retorno da chamada	
+	## Testa a abertura do arquivo:
+	add $t1, $zero, $t0	 	# armazena em $t1 o valor de retorno da chamada 
+	bltz $t1, encerraPrograma 	# se o retorno da chamada for negativo, houve erro na abertura do arquivo
+	
+	lw $a0, 0($t0)			# armazena o valor do descritor do arquivo em $a0 
+	jr	$ra
+	
+aberturaArquivoData:
+	la $a0, local_arquivo_data 	  # carrega o endereço do arquivo em $a0
+	li $a1, 0 		  	  # seta a flag do arquivo em 0 para modo de leitura
+	la $a0, local_arquivo_data 	  # carrega o nome do arquivo em $a0
+	li $v0, 13 		  	  # 0 = abre o arquivo em modo de leitura
+	syscall			  	  # faz a chamada de sistema n. 13 para abrir o arquivo
+	la $t0, descritor_arquivo 	  # armazena em $t0 o endereço do descritor do arquivo
+	sw $v0, 0($t0)		  	  # armazena em descritor_arquivo o valor de retorno da chamada	
+	## Testa a abertura do arquivo:
+	add $t1, $zero, $t0	 	  # armazena em $t1 o valor de retorno da chamada 
+	bltz $t1, encerraPrograma	  # se o retorno da chamada for negativo, houve erro na abertura do arquivo
+	
+	lw $a0, 0($t0)			  # armazena o valor do descritor do arquivo em $a0 
+	jr	$ra
+	
+leituraArquivoData:	
+	la $a1, buffer_leitura  	# carrega em $a1 o endereço da variável buffer_leitura
+	li $a2, 4			# carrega em $a2 o número de bytes que serão lidos do arquivo
+	li $v0, 14			# carrega em $v0 a chamada que será feita ao sistema (14 = leitura arquivo)
+	syscall
+	
+## Armazena o valor de buffer_leitura para memoria_text e incrementa o endereço de memoria_text
+	lw $a2, endereco_data		# carrega o endereço inicial de memoria_texto em $a2
+	lw $a3, 0($a1) 			# carrega o valor contido no buffer_leitura em $a3
+	sw $a3, 0($a2)			# armazena o valor do buffer (endereço tá em $a1) para o endereço de memoria_text
+	
+	la $s0, endereco_data 		# carrega o endereço da variável endereco_texto
+	addi $t1, $a2, 4		# incrementa 4 bytes no endereço de memoria
+	sw $t1, 0($s0)			# armazena o novo valor do endereco_texto
+## DO-WHILE:
+	bgtz $v0, leituraArquivoData 	# se o número de caracteres lidos for maior que 0, continua a leitura do arquivo
+	jr	$ra
+	
+leituraArquivo:	
+	la $a1, buffer_leitura  	# carrega em $a1 o endereço da variável buffer_leitura
+	li $a2, 4			# carrega em $a2 o número de bytes que serão lidos do arquivo
+	li $v0, 14			# carrega em $v0 a chamada que será feita ao sistema (14 = leitura arquivo)
+	syscall
+	
+## Armazena o valor de buffer_leitura para memoria_text e incrementa o endereço de memoria_text
+	lw $a2, endereco_texto		# carrega o endereço inicial de memoria_texto em $a2
+	lw $a3, 0($a1) 			# carrega o valor contido no buffer_leitura em $a3
+	sw $a3, 0($a2)			# armazena o valor do buffer (endereço tá em $a1) para o endereço de memoria_text
+	
+	la $s0, endereco_texto 		# carrega o endereço da variável endereco_texto
+	addi $t1, $a2, 4		# incrementa 4 bytes no endereço de memoria
+	sw $t1, 0($s0)			# armazena o novo valor do endereco_texto
+## DO-WHILE:
+	bgtz $v0, leituraArquivo 	# se o número de caracteres lidos for maior que 0, continua a leitura do arquivo
+	jr	$ra
+	
+fechaArquivo:
+	li $v0, 16
+	syscall
+	jr	$ra
+
+inicioBuscaInstrucao:	
+## Carrega em PC o endereço inicial de memoria_text
+	la $a0, PC 			## carrega endereço texto
+	la $a1, endereco_texto		## carrega o valor do endereço inicial de memoria_text pra $a1
+	sw $a1, 0($a0)			## armazena em PC o valor de $a1
+	jr	$ra
+	
+## busca_instrucao: seta os valores de PC e IR para serem executados
+buscaInstrucao:
+	la $a0, PC			## carrega endereço de PC em $a0	
+	lw $a1, 0($a0)			## carrega o valor de PC em $a1
+	lw $a2, 0($a1)			## carrega o valor armazenado no endereço de PC
+	
+	la $a3, IR			## carrega o endereço de IR em $a3
+	sw $a2, 0($a3)			## armazena a instrução na variável IR
+	
+	## Atualiza o PC para apontar para a próxima instrução
+	addi $a1, $a1, 4
+	sw $a1, 0($a0) 			## armazena o valor incrementado em PC
+
+	jr	$ra
+	
+############################################################################################################################	
+ # Mapa Campos da Instrução - Decodifica Instrução
+ # INSTRUÇÕES R
+ # $s0 = campo OPCODE
+ # $s1 = campo funct
+ # $s2 = campo shamt
+ # $s3 = campo rd
+ # $s4 = campo rt
+ # $s5 = campo rs
+ 
+ # INSTRUÇÕES I
+ # $s0 = campo OPCODE
+ # $s1 = campo Immediate
+ # $s4 = campo rt
+ # $s5 = campo rs
+
+ # INSTRUÇÕES J
+ # $s0 = campo OPCODE
+ # $s1 = campo Address
+
+decodificaInstrucao: 
+# ideia addi	$sp, $sp, -4
+# ideia sw	$ra, 0($sp)
+	lw $a0, IR
+
+	srl $s0, $a0, 26 			## $s0 = campo OPCODE
+	andi $s1, $a0, 0x0000003F 		## $s1 = campo FUNCT
+	srl $s2, $a0, 6 			## $s2 = campo shamt
+	andi $s2, $s2, 0x0000001F
+	srl $s3, $a0, 11 			## $s3 = campo rd
+	andi $s3, $s3, 0x0000001F
+	srl $s4, $a0, 16 			## $s4 = campo rt
+	andi $s4, $s4, 0x0000001F
+	srl $s5, $a0, 21 			## $s5 = campo rs
+	andi $s5, $s5, 0x0000001F
+	sll $s6, $s5, 1
+	## DIRECIONA PRO TIPO DE INSTRUÇÃO
+	beqz    $s0,    instrucaoR		# if opcode == 0 instrucao do tipo r
+	bge 	$s0, 4, instrucaoI		# else if opcode >= 4 instrucao do tipo i
+	j       instrucaoJ			# else instrucao do tipo j
+	
+# ideia lw	$ra, 0($sp)
+# ideia addi	$sp, $sp, 4
+	
+instrucaoR:
+	jr	$ra
+	
+instrucaoI:
+	sll $t0, $s3, 11 			## desloca campo rd 11 bits para esquerda
+	sll $t1, $s2, 6  			## desloca campo shamt 6 bits para esquerda
+	add $t2, $t1, $t0 			## soma shamt+rd				
+	add $s1, $t2, $s1			## soma shamt+rd+funct
+	jr	$ra
+	
+instrucaoJ:
+	sll $t0, $s5, 21  			## desloca campo rs 21 bits para esquerda
+	sll $t1, $s4, 16  			## desloca campo rt 16 bits para esquerda
+	sll $t2, $s3, 11 			## desloca campo rd 11 bits para esquerda
+	sll $t3, $s2, 6  			## desloca campo shamt 6 bits para esquerda
+	add $t4, $t0, $t1 			## $t4 -> rs+rt
+	add $t5, $t2, $t3 			## $t5 -> rd+shamt
+	add $t4, $t4, $t5 			## $t4 + $t5
+	sw  $t4, 0($s1)   			## $s1 -> address
+	jr	$ra	 
+
+executaInstrucao:
+
+	beqz    $s0,       comparaFunct
+	beq	$s0, 8,    instADDI
+	beq	$s0, 9,    instADDIU
+	beq	$s0, 0x1c, instMUL
+	beq	$s0, 5,    instBNE
+	beq	$s0, 2,    instJ
+	beq	$s0, 3,    instJAL
+	beq	$s0, 0x23,  instLW
+	beq	$s0, 0x2b,  instSW
+	beq	$s0, 0xf,   instLUI
+	beq	$s0, 0xd,  instORI
+	
+comparaFunct:
+	beq $s1, 0xc,  instSYS
+	beq $s1, 0x08, instJR
+	beq $s1, 0x20, instADD
+	beq $s1, 0x21, instADDU
+	beq $s1, 0xb   instMOVE
+	
+## add
+instADD: 
+#rd = rs + rt
+        ## rs
+	la $t0, regs     			## endereço inicial dos registradores
+	sll $t1, $s5, 2   			## 4 * n. do registrador rs
+	add $t1, $t1, $t0 			## endereço inicial de regs + 4 * n. do registrador
+	lw $t2, 0($t1)    			## $t2 = valor armazenado em regs[rs]
+	## rt
+	sll $t1, $s4, 2  			## 4 * n. do registrador rt
+	add $t1, $t1, $t0 			## endereço inicial de regs + 4 * n. do registrador
+	lw $t3, 0($t1)    			## $t3 = valor armazenado em regs[rt]
+	## rd
+	sll $t1, $s3, 2   			## 4 * n. do registrador de destino (rd)
+	add $t1, $t1, $t0 			## $t1 -> endereço do registrador rd
+	add $t4, $t3, $t2 			## $t4 = rs+rt
+	sw  $t4, 0($t1)
+	jr	$ra
+	
+## addu	
+instADDU:
+#rd = rs + rt (sem sinal)
+        ## rs
+	la $t0, regs      			## endereço inicial dos registradores
+	sll $t1, $s5, 2   			## 4 * n. do registrador rs
+	add $t1, $t1, $t0 			## endereço inicial de regs + 4 * n. do registrador
+	lw $t2, 0($t1)    			## $t2 = valor armazenado em regs[rs]
+	## rt
+	sll $t1, $s4, 2   			## 4 * n. do registrador rt
+	add $t1, $t1, $t0 			## endereço inicial de regs + 4 * n. do registrador
+	lw $t3, 0($t1)    			## $t3 = valor armazenado em regs[rt]
+	## rd
+	sll $t1, $s3, 2  			 ## 4 * n. do registrador de destino (rd)
+	add $t1, $t1, $t0 			## $t1 -> endereço do registrador rd
+	add $t4, $t3, $t2 			## $t4 = rs+rt
+	sw  $t4, 0($t1)
+	jr	$ra
+
+## addiu	
+instADDIU:
+	la $t0, regs      			## endereço inicial dos registradores
+	lw $t1, 0($s1)	  			## valor Imm armazenado
+	sll $t2, $s5, 2	  			## 4 * valor armazenado em rs
+	add $t3, $t2, $t0 			## endereço do registrador rs
+	lw $t2, 0($t3)    			## $t2 -> valor armazenado em rs
+	add $t2, $t2, $t1 			## rs+imm
+	## rt
+	sll $t1, $s4, 2	  			## 4 * valor armazenado em rt
+	add $t3, $t1, $t0 			## $t3 -> endereço do registrador rt
+	sw $t2, 0($t3)    			## registrador rt = rs +imm 
+	jr	$ra
+	
+## addi	
+instADDI:
+	la $t0, regs      			## endereço inicial dos registradores
+	lw $t1, 0($s1)	  			## valor Imm armazenado
+	sll $t2, $s5, 2	  			## 4 * valor armazenado em rs
+	add $t3, $t2, $t0 			## endereço do registrador rs
+	lw $t2, 0($t3)   			 ## $t2 -> valor armazenado em rs
+	add $t2, $t2, $t1 			## rs+imm
+	## rt
+	sll $t1, $s4, 2	  			## 4 * valor armazenado em rt
+	add $t3, $t1, $t0 			## $t3 -> endereço do registrador rt
+	sw $t2, 0($t3)    			## registrador rt = rs +imm 
+	jr	$ra
+	
+## sw
+instSW:
+	la  $t0, regs      			## endereço inicial dos registradores
+	sll $t1, $s4, 2	   			## $t1 -> valor do campo rt * 4
+	add $t1, $t1, $t0  			## $t1 -> endereço efetivo do registrador rt
+	sll $t2, $s5, 2   			 ## $t2 -> valor do campo rs * 4
+	add $t2, $t2, $t0  			## $t2 -> endereço efetivo do registrador rs
+	lw  $t3, 0($s1)    			## $t3 -> valor do campo immediate
+	add $t3, $t3, $t2 			 ## $t3 -> imm + rs
+	lw  $t0, 0($t1)
+	sw  $t0, 0($t3)    			## armazena o valor de rt no endereço imm + rs
+	jr	$ra
+	
+## lw
+instLW:
+	la $t0, regs 
+	sll $t1, $s4, 2	  			 ## $t1 -> valor do campo rt * 4
+	add $t1, $t1, $t0  			## $t1 -> endereço efetivo do registrador rt
+	sll $t2, $s5, 2    			## $t2 -> valor do campo rs * 4
+	add $t2, $t2, $t0 			 ## $t2 -> endereço efetivo do registrador rs
+	lw  $t3, 0($s1)   			 ## $t3 -> valor do campo immediate
+	add $t3, $t3, $t2  			## $t3 -> imm + rs
+	lw  $t1, 0($t3)
+	jr	$ra
+
+## lui
+instLUI:
+	la $t0, regs      			 ## endereço inicial dos registradores
+    	sll $t1, $s5, 2    			## $t1 -> valor do campo rt * 4
+    	add $t2, $t1, $t0  			# $t2 -> Endereço base + rt 
+    	andi $t3, $s1, 0x00FF			# $t3 -> 8 bits menos significativos de imm
+    	sw $t3, 0($t2)				# Armazena no endereço efetivo do registrador rt
+    	jr	$ra
+    	
+ ## ori  	
+instORI:
+	la $t0, regs      			 ## endereço inicial dos registradores
+    	sll $t1, $s5, 2    			## $t1 -> valor do campo rs * 4
+    	add $t2, $t1, $t0  			# $t2 -> Endereço base + rs
+    	lw $t3, 0($t2)				## $t3 -> Valor armazenado em rs 
+    	or $t4, $t3, $s1			## $t4 -> or = rs e imm
+    	sll $t1, $s4, 2    			## $t1 -> valor do campo rt * 4
+    	add $t1, $t1, $t0  			## Endereço base + rt
+    	sw $t3, 0($t1)    			 # Armazena resultado de or no endereço efetivo de rt
+    	jr	$ra
+    	
+## move
+instMOVE:
+	la $t0, regs
+    	sll $t1, $s5, 2    			## $t1 -> valor do campo rt * 4
+    	add $t1, $t1, $t0  			## Calcula o endereço do rt
+    	lw $t5, 0($t1)     			## $t5 -> Carrega o valor do rt $t5
+    	sll $t3, $s4, 2    			## $t3 -> valor do campo rs * 4
+    	add $t3, $t3, $t0  			## Endereço base + rs
+    	sw $t5, 0($t3)     			## Armazena o valor de rt em rs
+    	jr	$ra
+
+## bne
+instBNE:
+	la  $t0, regs
+	sll $t1, $s5, 2      			## $t1 -> valor do campo rt * 4
+    	add $t1, $t1, $t0    			## Calcula o endereço do rt
+    	lw  $t5, 0($t1)      			## $t5 -> Carrega o valor do rt $t5
+    	sll $t3, $s4, 2      			## $t3 -> valor do campo rs * 4
+    	add $t3, $t3, $t0    			## Calcula o endereço do rs
+    	lw  $t4, 0($t3)      			## $t4 -> Carrega o valor do rs
+    	
+    	beq $t4, $t3, fimBNE 			## se valores forem iguais, encerra a instrução e retorna pra main
+   
+   	lw $t0, PC	    			 ## $t0 -> valor de PC
+   	lw $t1, 0($s1)       			## $t1 -> valor do immediate
+   	sll $t1, $t1, 2	     			## Imm * 4 -> deslocamento de 4 bytes
+   	add $t2, $t0, $t1    			## $t2 -> PC + n. de instruções a serem deslocadas
+   	sw $t2, PC	
+    	
+fimBNE:
+	jr	$ra
+
+## mul	
+instMUL:
+	la  $t0, regs
+	sll $t1, $s5, 2     			 ## $t1 -> valor do campo rt * 4
+    	add $t1, $t1, $t0    			## Calcula o endereço do rt
+    	lw  $t5, 0($t1)     			 ## $t5 -> Carrega o valor do rt $t5
+    	sll $t3, $s4, 2      			## $t3 -> valor do campo rs * 4
+    	add $t3, $t3, $t0    			## Calcula o endereço do rs
+    	lw  $t4, 0($t3)      			## $t4 -> Carrega o valor do rs 
+    	
+    	sll $t1, $s3, 2      			## $t1 -> valor do campo rd * 4
+    	add $t1, $t1, $t0    			## Calcula o endereço do rd
+    	mul $t2, $t4, $t5    			## $t2 -> multiplicação de rs+rt
+    	sw $t2, 0($t1)       			## armazena o resultado em RD
+    	
+    	mfhi 	$t0	     			# $t0 -> valor de hi
+    	la	$t1, HI	     
+    	sw	$t0, 0($t1)  			# Insere $t0 no endereço de hi 
+    	mflo 	$t2	    			# $t2 -> valor de lo
+    	la	$t3, LO	     
+    	sw	$t2, 0($t3)  			# Insere $t2 no endereço de lo
+    	
+    	jr	$ra
+
+## j	
+instJ:
+	lw	$t0, 0($s1)			# $t0 -> address (26 bits) 
+    	la	$t1, PC				# $t1 -> endereço de PC 
+    	lw	$t2, endereco_texto		# $t2 -> endereço inicial da memória de texto
+    	sll	$t0, $t0, 2			# $t0 -> address * 4
+    	add 	$t0, $t0, $t2			# $t0 <- endereço efetivo da label desejada
+    	sw	$t0, 0($t1)			# Armazena em PC o endereço da instrução definida pela label
+    	
+    	jr $ra
+    	
+## jal   	
+instJAL:
+	la $t0, regs				 ## endereço inicial dos registradores
+	lw $t1, PC				 ## endereço armazenado em PC
+	sw $t1, 31($t0)				 ## armazena endereço da próxima instrução em $ra 
+	
+	j instJ
+
+## jr
+instJR:
+	la $t0, regs				 ## endereço inicial dos registradores
+	lw $t1, 124($t0)			 ## endereço armazenado em $ra
+	la $t2, PC				 ## endereço armazenado em PC
+	sw $t1, 0($t2)				 ## armazena em PC endereço armazenado em $ra 
+
+## syscall	
+instSYS:
+	la $t0, regs				 ## endereço inicial dos registradores
+	lw $t1, 8($t0)				 ## $t1 -> valor armazenado no regs $v0
+	sw $t1, ($v0)				 ## Armazena o valor em $v0
+	syscall
+    	
+##############################################
+encerraPrograma:
+	li $v0, 17
+	syscall			# faz a chamada 17 e encerra o programa	
+
+
+.data
+	PC: 			.word 0
+	IR: 			.word 0
+	HI:			.word 0
+	LO:			.word 0
+	regs: 			.space 128 	  ## Registradores do simulador
+	buffer_leitura: 	.space 4          ## Buffer para leitura do arquivo de entrada. Armazena 4 bytes por vez
+	memoria_text: 		.space 1024
+	memoria_data: 		.space 1024
+	memoria_pilha:		.space 1024
+	endereco_texto: 	.word 0x1001008c
+	endereco_data:  	.word 0x10010000
+	endereco_pilha:		.word 0x7FFFEFFC
+	local_arquivo:  	.asciiz "trabalho_01-2024_1.bin"
+	local_arquivo_data: 	.asciiz "trabalh0_01-2024_1.dat"
+	descritor_arquivo: 	.word 0
+	count:  .word 0    # Variável para contar as iterações do loop
+	
